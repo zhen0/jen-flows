4f4196bc6e642d9be7cae5d2e1ffd8945408f042
Jennifer Grange <jenny@prefect.io>
2023-03-30 10:10:11 -0400

    breaking artifacts

--- artifactsFlow.py
+++ artifactsFlow.py
@@ -1,23 +1,22 @@
-from prefect.experimental.artifacts import create_table_artifact, create_link_artifact, create_markdown_artifact
+from prefect.artifacts import create_table_artifact, create_link_artifact, create_markdown_artifact
 from prefect import task, flow
+import markdown
 
-@task(persist_result=True)
-def my_fn():
-    highest_churn_possibility = [
-       {'customer_id':'12345', 'name': 'John Smith', 'churn_probability': 0.85 }, 
-       {'customer_id':'56789', 'name': 'Jane Jones', 'churn_probability': 0.65 } 
-    ]
-
-    artId= create_table_artifact(
+@task()
+def table_task():
+    highest_churn_possibility = {
+        'customer_id': ['123456789', '987654321', '246810121', '135791113', '864208046'],
+        'name': ['John Smith', 'Jane Doe', 'Bob Johnson', 'Sarah Jones', 'Tom Wilson'],
+        'churn_probability': [0.85, 0.79, 0.67, 0.61, 0.57]
+    }
+    create_table_artifact(
         key="personalized-reachout",
         table=highest_churn_possibility,
         description= "# Marvin, please reach out to these customers today!"
     )
-    return artId
 
-
-@task(persist_result=True)
-def my_special_task(persist_result=True):
+@task()
+def link_task():
     artifact_id = create_link_artifact(
         key="my-important-link-2",
         link="s3://my-bucket/my-key",
@@ -26,13 +25,47 @@ def my_special_task(persist_result=True):
     )
     return artifact_id
 
-@flow(persist_result=True)
-def my_flow(persist_result=True):
-    my_special_task()
-    my_fn()
+@task
+def my_task():
+    na_revenue = 500000
+    markdown_report = f"""
+        # Sales Report
+
+        ## Summary
+
+        In the past quarter, our company saw a significant increase in sales, with a total revenue of $1,000,000. This represents a 20% increase over the same period last year.
+
+        ## Sales by Region
+
+        | Region        | Revenue |
+        |:--------------|-------:|
+        | North America | 5000 |
+        | Europe        | $250,000 |
+        | Asia          | $150,000 |
+        | South America | $75,000 |
+        | Africa        | $25,000 |
+
+        ## Top Products
+
+        1. Product A - $300,000 in revenue
+        2. Product B - $200,000 in revenue
+        3. Product C - $150,000 in revenue
+
+        ## Conclusion
+
+        Overall, these results are very encouraging and demonstrate the success of our sales team in increasing revenue across all regions. However, we still have room for improvement and should focus on further increasing sales in the coming quarter.
+        """
+    return markdown_report
+
+@flow()
+def my_flow():
+    # table_task()
+    # link_task()
+    my_important_report = my_task()
+    # report = markdown_task(),
     create_markdown_artifact(
         key="important-markdown-2",
-        markdown="# List of important ducks: 1. Marvin",
+        markdown=my_important_report,
         description="Everything you need to know",
     )
 
