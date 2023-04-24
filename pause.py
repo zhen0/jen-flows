f2a48013f22459ffff2a6cd90f24b2c73e2fae8a
Jennifer Grange <jenny@prefect.io>
2023-04-24 16:44:38 -0400

    add persist result

--- pause.py
+++ pause.py
@@ -21,16 +21,16 @@ def convert_str_to_int(num: str):
     return int(num)
 
 from prefect.engine import pause_flow_run
-@flow()
+@flow(persist_result=True)
 def pause_example(num: str, num2: int = 30):
     x = append_1.submit(num, num2)
     y = append_2.submit(x.result(), num2)
     z = append_num.submit(y.result(), "12", num2)
     logger = get_run_logger()
     logger.info(f"First result: {convert_str_to_int(x.result())}")
-    # pause_flow_run(timeout=30)
+    # pause_flow_run(timeout=300)
     logger.info(f"Second result: {convert_str_to_int(y.result())}")
     logger.info(f"Third result: {convert_str_to_int(z.result())}")
 
-pause_example(30)
+# pause_example(30)
 # The line pause_flow_run(timeout=30) is commented out, but we'll come back to this later. This is a minimal flow that, when run perfectly, will generate the DAG attached in the bug summary section.
